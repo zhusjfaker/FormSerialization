3cd4115351ac86fb5375b20095efed3e7e451ac5
椛船 <jayson.zsj@alibaba-inc.com>
2018-11-05 14:46:08 +0800

    zhusj

--- index.ts
+++ index.ts
@@ -1,17 +1,106 @@
-import { ObjectFormSerializable } from "./core"
-
-const a = {
-    a: 1,
-    b: {
-        c: 123,
-        d: "dafd"
-    },
-    c: [1, 2, 3],
-    d: [
-        { oo: 123, hk: null },
-        { oo: 123, hk: null }
-    ]
-};
-
-let result = ObjectFormSerializable.Parse(a);
-console.log(result);
+export class ObjectFormSerializable {
+    public static Parse(data: any, isencode: boolean = true): string {
+        let obj = "";
+        let key: string[] = [];
+        if (Array.isArray(data)) {
+            for (let i = 0; i < data.length; i++) {
+                switch (typeof data[i]) {
+                    case "string":
+                        key.push("[" + i + "]");
+                        break;
+                    case "number":
+                        key.push("[" + i + "]");
+                        break;
+                    case "object":
+                        if (data[i] != null) {
+                            this.Convert(data[i], key, "[" + i + "]");
+                        }
+                        else {
+                            key.push("[" + i + "]");
+                        }
+                        break;
+                }
+            }
+        }
+        else {
+            this.Convert(data, key, "");
+        }
+        /** 默认按照key名称进行升序排序 */
+        key.sort();
+        /** 获取所有key值 */
+        if (Array.isArray(data)) {
+            key.every(x => {
+                if (eval('data' + x) === null || eval('data' + x) === undefined) {
+                    obj += "&" + x + "=";
+                }
+                else if (eval('data' + x) != null && typeof eval('data' + x) == "number") {
+                    obj += "&" + x + "=" + eval('data' + x);
+                }
+                else if (eval('data' + x) != null && typeof eval('data' + x) == "string") {
+                    obj += "&" + x + "=" + (isencode ? encodeURI(eval('data' + x)) : eval('data' + x));
+                }
+                return true;
+            });
+        }
+        else {
+            key.every(x => {
+                if (eval('data.' + x) === null || eval('data.' + x) === undefined) {
+                    obj += "&" + x + "=";
+                }
+                else if (eval('data.' + x) != null && typeof eval('data.' + x) == "number") {
+                    obj += "&" + x + "=" + eval('data.' + x);
+                }
+                else if (eval('data.' + x) != null && typeof eval('data.' + x) == "string") {
+                    obj += "&" + x + "=" + (isencode ? encodeURI(eval('data.' + x)) : eval('data.' + x));
+                }
+                return true;
+            });
+        }
+        return obj;
+    }
+    private static Convert(data: any, key: string[], top?: string): void {
+        Object.keys(data).every(x => {
+            let perfix = top != "" ? top + "." : "";
+            switch (typeof data[x]) {
+                case "string":
+                    key.push(perfix + x);
+                    break;
+                case "number":
+                    key.push(perfix + x);
+                    break;
+                case "function":
+                    break;
+                case "undefined":
+                    key.push(perfix + x);
+                    break;
+                case "object":
+                    if (data[x] != null) {
+                        if (Array.isArray(data[x])) {
+                            for (let i = 0; i < data[x].length; i++) {
+                                switch (typeof data[x][i]) {
+                                    case "string":
+                                        key.push(perfix + x + "[" + i + "]");
+                                        break;
+                                    case "number":
+                                        key.push(perfix + x + "[" + i + "]");
+                                        break;
+                                    case "object":
+                                        this.Convert(data[x][i], key, perfix + x + "[" + i + "]");
+                                        break;
+                                }
+                            }
+                        }
+                        else {
+                            this.Convert(data[x], key, perfix + x);
+                        }
+                    }
+                    else {
+                        key.push(perfix + x);
+                    }
+                    break;
+            }
+            return true;
+        });
+        return;
+    }
+}
